c1f8b564062b7b3120d3bc0e1c9a752944c41514
Jacc178 <62754107+Jacc178@users.noreply.github.com>
2020-11-18 17:13:53 +0530

    Add files via upload

--- RPS.py
+++ RPS.py
@@ -1,46 +1,55 @@
 #Rock Paper Scissor 
 import random
+def play():
+    player_score = 0
+    computer_score = 0
+    n = int(input("Enter number of rounds: "))
 
-player_score = 0
-computer_score = 0
-n = int(input("Enter number of rounds: "))
+    def game():
+        nonlocal player_score
+        nonlocal computer_score
+        print("\nROUND", i, "\n")
 
-def game():
-    global player_score
-    global computer_score
-    print("ROUND", i)
+        choices = ["rock", "paper", "scissor"]
+        comp_choice = random.choice(choices).title()
+        player_choice = str(input("Enter choice: ")).lower()
 
-    choices = ["rock", "paper", "scissor"]
-    comp_choice = random.choice(choices).title()
-    player_choice = str(input("Enter choice: ")).lower()
+        print("Computer choice:", comp_choice)
 
-    print("Computer choice:", comp_choice)
+        if ((player_choice == "rock" and comp_choice == "Paper") 
+        or (player_choice == "paper" and comp_choice == "Scissor")
+        or (player_choice == "scissor" and comp_choice == "Rock")):
+            computer_score += 1
+            print("\nResult: Computer wins round", i, "\n")
 
-    if ((player_choice == "rock" and comp_choice == "Paper") 
-    or (player_choice == "paper" and comp_choice == "Scissor")
-    or (player_choice == "scissor" and comp_choice == "Rock")):
-        computer_score += 1
-        print("Result: Computer wins round", i)
+        elif ((comp_choice == "Rock" and player_choice == "paper") 
+        or (comp_choice == "Paper" and player_choice == "scissor")
+        or (comp_choice == "Scissor" and player_choice == "rock")):
+            player_score += 1
+            print("\nResult: Player wins round", i, "\n")
+        else:
+            print("Result: Tie")
+        print("Player Score:", player_score, "\n" "Computer Score:", computer_score)
 
-    elif ((comp_choice == "Rock" and player_choice == "paper") 
-    or (comp_choice == "Paper" and player_choice == "scissor")
-    or (comp_choice == "Scissor" and player_choice == "rock")):
-        player_score += 1
-        print("Result: Player wins round", i)
-    else:
-        print("Result: Tie")
-    print("Player Score:", player_score, "\n" "Computer Score:", computer_score)
-    
-for i in range(1,n+1):
-    game()
+    def results():
+        print("\n\nGAME OVER")
+        print("Player Score:", player_score, "\n" "Computer Score:", computer_score)
 
-print("GAME OVER")
-print("Player Score:", player_score, "\n" "Computer Score:", computer_score)
+        if player_score > computer_score:
+            print("\nPLAYER WINS")
+        elif computer_score > player_score:
+            print("\nCOMPUTER WINS")
+        else:
+            print("\nTIE")
 
-if player_score > computer_score:
-    print("PLAYER WINS")
-elif computer_score > player_score:
-    print("COMPUTER WINS")
+    for i in range(1,n+1):
+        game()
+    results()
+
+play()
+
+replay = input("Play Again? (Y/N): ").upper()
+if replay == "Y":
+    play()
 else:
-    print("TIE")
-
+    print("\nThanks for playing")
